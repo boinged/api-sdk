7a31780a33b4508df37dd39ff52d4f20a2be9d38
boinged <stevenshipton@hotmail.com>
2021-07-17 13:30:33 +0000

    update deps

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ContentSDK = void 0;
 var contentSDK_1 = require("./sdk/contentSDK");
 Object.defineProperty(exports, "ContentSDK", { enumerable: true, get: function () { return contentSDK_1.ContentSDK; } });
